088b5b9b41a02109d938d7504427debc5fd16370
Krzysztof Karaś <krzychu12350@interia.pl>
2022-10-06 23:38:55 +0200

    initial commit

---
+++ frontend/src/views/FetchedDataView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>This is data fetched from the Laravel API</h1>
+    {{ person.id }} {{ person.name }} {{ person.surname }}
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "HomeView",
+  data() {
+    return {
+      person: Array,
+    };
+  },
+  mounted() {
+    this.fetchTestData();
+  },
+  methods: {
+    fetchTestData() {
+      this.axios
+        .get("http://127.0.0.1:8000/api/test-endpoint")
+        .then((response) => {
+          this.person = response.data.data;
+          console.log(this.person);
+        });
+    },
+  },
+});
+</script>
