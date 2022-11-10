5e02e394c00394020f4df365754e97d5d44f2393
Krzysztof Karaś <krzychu12350@interia.pl>
2022-11-10 06:40:02 +0100

    Created password reseting email template

--- backend/app/Mail/SendMail.php
+++ backend/app/Mail/SendMail.php
@@ -10,7 +10,9 @@ use Illuminate\Queue\SerializesModels;
 class SendMail extends Mailable
 {
     use Queueable, SerializesModels;
+
     public $token;
+
     /**
      * Create a new message instance.
      *
@@ -28,8 +30,17 @@ class SendMail extends Mailable
      */
     public function build()
     {
-        return $this->markdown('Email.resetPassword')->with([
-            'token' => $this->token
-        ]);
+        $img_url = 'logo.png';
+        return $this
+            /*
+            ->attach('E:\Zlecenia\ai2-2022-projekt-lab2-z4\frontend\src\assets\logo.png', [ //absolute path
+                'as' => 'logo.png',
+                'mime' => 'images/png'
+            ])
+            */
+                //->attach(public_path('/logo.png'))
+            ->view('Email.resetPassword')->with([
+                'token' => $this->token,
+            ]);
     }
 }
